e69edc7c1bee7aaad1e262c4a9c3209166f95bbd
aznjp <jpark103193@gmail.com>
2020-09-20 20:28:25 -0400

    added in moment.js to change information structure for dates and time and redid css dom manipulation to make application more aesthetically pleasing:

--- assets/js/events.js
+++ assets/js/events.js
@@ -6,7 +6,7 @@ async function getEventInfo(city) {
     var response = await fetch(eventURL)
 
     var data = await response.json()
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 6; index++) {
 
         var eventName = data._embedded.events[index].name
 
@@ -19,7 +19,7 @@ async function getEventInfo(city) {
 
         var newCard = $("<div>").attr(
             "class",
-            "card column is-one-fourth is-one-tablet is-rounded box mt-6 mb-0 my-4 has-text-centered is-desktop "
+            "card column is-one-third is-full-mobile is-half-desktop is-rounded box mt-6 mb-0 my-4 has-text-centered"
         );
         $("#eventList").append(newCard);
 
@@ -52,22 +52,25 @@ async function getEventInfo(city) {
         $(newCard).append(newCardFooter);
 
 
-        newCardHeader.append($("<h2>").html("<strong>" + eventName + "</strong>").attr("class", "is-size-2"));
-        newCardContent1.append($("<h3>").html("<strong> Activity: </strong>" + eventType).attr("class", "is-size-3"));
-        newCardContent1.append($("<h4>").html("<strong> Genre: </strong>" + eventGenre).attr("class", "is-size-3"));
+        newCardHeader.append($("<h2>").html("<strong>" + eventName + "</strong>").attr("class", "is-size-3-desktop is-size-4"));
+        newCardContent1.append($("<h3>").html("<strong> Activity: </strong>" + eventType).attr("class", "is-size-3-desktop is-size-5"));
+        newCardContent1.append($("<h4>").html("<strong> Genre: </strong>" + eventGenre).attr("class", "is-size-3-desktop is-size-5"));
 
         for (let j = 0; j < 10; j++) {
             // const element = array[index];
             var imageItemRatio = data._embedded.events[index].images[j].ratio
-            if (imageItemRatio === "4_3") {
+            if (imageItemRatio === "3_2") {
                 console.log(imageItemRatio)
                 var eventImage = data._embedded.events[index].images[j].url
                 newCardContentImg.append($("<img>").attr("src", eventImage));
+                break
             }
         }
 
         var eventStartDate = data._embedded.events[index].dates.start.localDate
+        var eventStartDateFormat = moment(eventStartDate, 'YYYY-MM-DD').format('dddd, MMM Do YYYY')
         var eventStartTime = data._embedded.events[index].dates.start.localTime
+        var eventStartTimeFormat = moment(eventStartTime, 'HH:mm:ss').format('h:mm a')
 
         var eventAddress = data._embedded.events[index]._embedded.venues[0].address.line1
         var eventCity = data._embedded.events[index]._embedded.venues[0].city.name
@@ -81,15 +84,15 @@ async function getEventInfo(city) {
         var eventTicketURL = data._embedded.events[index].url
 
 
-        newCardContent2.append($("<p>").html("<strong>Date: </strong>" + eventStartDate).attr("class", "is-size-6"));
-        newCardContent2.append($("<p>").html("<strong>Time: </strong>" + eventStartTime).attr("class", "is-size-6"));
+        newCardContent2.append($("<p>").html("<strong>Date: </strong>" + eventStartDateFormat).attr("class", "is-size-6"));
+        newCardContent2.append($("<p>").html("<strong>Time: </strong>" + eventStartTimeFormat).attr("class", "is-size-6"));
         newCardContent2.append($("<p>").html("<strong>Location: </strong>" + eventAddress + "</br> " + eventCity + ", " + eventState).attr("class", "is-size-6"));
         newCardContent2.append($("<p>").html("<strong>Venue Name: </strong>" + eventAddressName).attr("class", "is-size-6"));
         // newCardContent.append($("<li>").html("<strong>Price: </strong>" + eventPriceMin + " - " + eventPriceMax + " " + eventCurrencyType).attr("class", "is-size-5"));
 
         newCardFooter.append($("<a>").html("<strong>Ticket Links</strong>").attr({
             "href": eventTicketURL,
-            "class": "is-size-3"
+            "class": "is-size-4"
         }));
 
     }
